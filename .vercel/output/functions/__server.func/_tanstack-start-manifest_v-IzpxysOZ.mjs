aee76954159526eb03e590bccd41518d34affc94
urlsab <yairsabag213@gmail.com>
2026-06-16 11:13:35 +0300

    remove committed vercel build output

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-IzpxysOZ.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/uysCodes/meirav-s-safe-harbor/src/routes/__root.tsx", children: ["/", "/privacy", "/terms"], preloads: ["/assets/index-DOayteAm.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-DOayteAm.js" } }] }, "/": { filePath: "C:/uysCodes/meirav-s-safe-harbor/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-B8qwCF4P.js"] }, "/privacy": { filePath: "C:/uysCodes/meirav-s-safe-harbor/src/routes/privacy.tsx", children: void 0, preloads: ["/assets/privacy-DrqI6ful.js"] }, "/terms": { filePath: "C:/uysCodes/meirav-s-safe-harbor/src/routes/terms.tsx", children: void 0, preloads: ["/assets/terms-BmkhsOoi.js"] } } });
-export {
-  tsrStartManifest
-};
